0f6beae30fcb81d792ddd06648d84b8890b65269
Laura Brogan <47229870+LauraBrogan@users.noreply.github.com>
2020-12-06 22:09:25 +0000

    Code updated 06/12/20

--- PersonDao.py
+++ PersonDao.py
@@ -1,41 +1,43 @@
 import mysql.connector
+import dbconfig as cfg
 from mysql.connector import cursor
 
 class PersonDao:
     db = ""
     def __init__(self):
         self.db = mysql.connector.connect(
-            host = 'localhost',
-            user = 'root',
-            password = 'Brogan33',
-            database = 'holidays'
+            
+           host=        cfg.mysql['host'],
+           user=        cfg.mysql['user'],
+           password=    cfg.mysql['password'],
+           database=    cfg.mysql['database']
         )
         #print("connection made")
 
-    def create(self, newperson):
-        cursor = self.db.cursor()
-        sql = "insert into person (personid, personname, age) values (%s, %s, %s)"
-        values = [
-           newperson['personid'],
-           newperson['personname'],
-           newperson['age']
-        ]
-        cursor.execute(sql, values)
-        self.db.commit()
-        return cursor.lastrowid
+    def create(self, person):
+       cursor = self.db.cursor()
+       sql = "insert into person (personid, personname, age) values (%s, %s, %s)"
+       values =[ 
+       person['personid'],
+       person['personname'],
+       person['age']
+       ]
+       cursor.execute(sql, values)
+       self.db.commit()
+       return cursor.lastrowid
 
     def getAll(self):
-         cursor = self.db.cursor()
-         sql = 'select * from person'
-         cursor.execute(sql) 
-         results = cursor.fetchall()
-         returnArray=[]
-         #print(results)
-         for result in results:
+        cursor = self.db.cursor()
+        sql = 'select * from person'
+        cursor.execute(sql) 
+        results = cursor.fetchall()
+        returnArray=[]
+        #print(results)
+        for result in results:
             resultAsDict = self.convertToDict(result)
             returnArray.append(resultAsDict)
 
-         return returnArray
+        return returnArray
 
     def findByID(self,personid):
         cursor = self.db.cursor()
@@ -50,10 +52,10 @@ class PersonDao:
         cursor = self.db.cursor()
         sql = "update person set personname = %s, age = %s where personid = %s"
         values = [
-           person['personname'],
-           person['age'],
-           person['personid']
-       ]
+            person['personname'],
+            person['age'],
+            person['personid']
+        ]
         cursor.execute(sql,values)
         self.db.commit()
         return person
@@ -67,14 +69,14 @@ class PersonDao:
         return{}
 
     def convertToDict(self,result):
-        colnames= ['personid', 'personname','age']
-        person = {}
-        
-        if result:
+         colnames = ['personid', 'personname','age']
+         person = {}
+         
+         if result:
             for i, colName in enumerate(colnames):
-                 value = result[i] 
-                 person[colName]= value
-        return person
+                value = result[i] 
+                person[colName] = value
+         return person
 
 personDao = PersonDao()
 
